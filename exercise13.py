5960f3448d34ef26f819b215b4a5b7082cf36502
michaelwaldron <34780784+michaelwaldron@users.noreply.github.com>
2017-12-27 14:07:40 -0500

    Add files via upload

--- exercise13.py
+++ exercise13.py
@@ -5,17 +5,25 @@ Practice Python Exercise 13
 Ask user how many Fibonacci numbers he wants, then generate them.
 
 """
+def gen_fib(size):
+    if size == 0:
+        fib = []
+        return fib
+    elif size == 1:
+        fib = [0]
+        return fib
+    elif size == 2:
+        fib = [0,1]
+        return fib
+    else:
+        i = 0
+        fib = [0,1]
+        while i + 2 < size:
+            fib_next = fib[i+1] + fib[i]
+            fib.append(fib_next)
+            i+=1
+        return fib
 
-def next_num(current_num, previous_num):
-    holder = previous_num + current_num
-    return holder
-
-amount = int(input("Enter the amount of Fibonacci numbers you want:"))
-fib_series = [0,1]
-tracker = 2
-while tracker < amount:
-    current_num = fib_series[tracker-1]
-    previous_num = fib_series[tracker-2]
-    fib_series.append(next_num(current_num, previous_num))
-    tracker += 1
-print("Fibonacci series up to", amount, "elements:", fib_series)
+user_in = int(input("How many Fibonacci numbers would you like?"))
+fib = gen_fib(user_in)
+print("The first", user_in, "Fibonacci numbers are:", fib)

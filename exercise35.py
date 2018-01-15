671a7a0aba1ec68b9aadafe24aee005bd2986c76
michaelwaldron <34780784+michaelwaldron@users.noreply.github.com>
2018-01-15 13:19:07 -0500

    Add files via upload

--- exercise35.py
+++ exercise35.py
@@ -6,6 +6,21 @@ Print number of birthdays in each month
 
 """
 
+num_to_string = {
+	1: "January",
+	2: "February",
+	3: "March", 
+	4: "April",
+	5: "May",
+	6: "June",
+	7: "July",
+	8: "August",
+	9: "September",
+	10: "October",
+	11: "November",
+	12: "December"
+}
+
 import json
 
 birthday = {}
@@ -25,4 +40,4 @@ for key in birthday:
         months[index] += 1
 
 for month in months:
-    print(months[month], 'birthday(s) in month', month)
+    print(months[month], 'birthday(s) in', num_to_string[month])
